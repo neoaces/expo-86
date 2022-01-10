f134a18dc156847502bbdb2f85344762f541665b
anzlechavezz <anzlemchavez@gmail.com>
2022-01-10 11:31:38 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './App.scss';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+
 
 function App() {
+  useEffect(
+    () => {
+      gsap.from('blockquote', {
+        duration: 1,
+        x: 200,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: '.intro',
+          start: "top top",
+          scrub: 1,
+          
+        }
+      })
+    }
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="titlebar">
+        <nav>
+          <ul>
+            <a href="">
+              <li>Home</li>
+            </a>
+            <a href="">
+              <li>Map</li>
+            </a>
+          </ul>
+        </nav>
+      </div>
+      <section className='intro'>
+        <h1>Museum Welcome</h1>
+      </section>
+
+      <section className='sticky'>
+        <blockquote>
+          Bonjour, et bienvenue à Expo 86.
+        </blockquote>
+      </section>
+
+      <section className='conclusion section'>
+        <h1>Museum Conclusion</h1>
+      </section>
     </div>
   );
 }
+
+
 
 export default App;
